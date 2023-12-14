ee03013abf5d87047b2c28467a4c135f4ceb641f
chandu <chandu@devops.com>
2023-12-14 16:19:24 -0600

    commited

---
+++ mongodb.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+ if [ $1 -ne 0 ]
+    then
+        echo -e "$R ERROR:: $2 FAILED"
+        
+    else
+        echo -e "$G  $2 i was installed it suceesesfully $N"
+    fi
+}
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: FAILED"
+    
+else
+    echo -e "$R i was sucessfully installed"
+fi
+
+cp mongo.repo /etc/yum.repos.d/
+
+VALIDATE $? "copied sucessfully"
