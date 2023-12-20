4e84c1e7c1eb5974e1db820ff0fdc5ad24911976
chandu <chandu@devops.com>
2023-12-19 19:00:54 -0600

    done done

--- roboshop.sh
+++ roboshop.sh
@@ -6,7 +6,7 @@ INSTANCES=("mongodb" "redis" "mysql" "user" "cart" "payment" "shipping" "dispatc
 
 for i in "${INSTANCES[@]}"
 do
-  echo "instance is: $i"
+  
     if [ $i == "mongodb" ]||[ $i == "mysql" ]||[ $i == "shipping"]
       then 
          INSTANCE_TYPE="t3.small"
@@ -14,5 +14,6 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
- aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-0d9d67af01c6a1199 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-0d9d67af01c6a1199 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+echo "$i: $IP_ADDRESS"
 done
