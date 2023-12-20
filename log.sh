d4cbdc64c3367303ffdc08d65738ca983db46cbe
chandu <chandu@devops.com>
2023-12-20 17:15:36 -0600

    done done

---
+++ log.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+ SOURCE_DIR="/tmp/shell-script"
+
+ R="\e[31m"
+ G="\e[32m"
+ Y="\e[33m"
+ N="\e[0m"
+
+ if [ ! -d $SOURCE_DIR ]
+   then 
+      echo -e "$R source directory dosn't exist"
+fi 
+
+FILES_TO_DELETE=$(find . -type -f -mtime +7 --name "*.log")
+
+while IFS= read -r line
+do
+   echo "DEleting file: $line"
+   
+done <<< $FILES_TO_DELETE
+
+
+
