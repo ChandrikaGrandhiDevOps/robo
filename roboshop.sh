3a237a3dc6170eb26f1a2887c46855cd1c23e013
chandu <chandu@devops.com>
2023-12-24 19:05:38 -0600

    helo

--- roboshop.sh
+++ roboshop.sh
@@ -4,7 +4,7 @@ AMI=ami-03265a0778a880afb
 SG_ID=sg-0d9d67af01c6a1199 
 INSTANCES=("mongodb" "redis" "mysql" "user" "cart" "payment" "shipping" "dispatch" "catalogue" "web")
 ZONE_ID=Z03096722Q6JHG4T1T97R
-DOMAIN_NAME="crobo.sh"
+DOMAIN_NAME="crobo.shop"
 for i in "${INSTANCES[@]}"
 do
     if [ $i == "mongodb" ]||[ $i == "mysql" ]||[ $i == "shipping" ]
@@ -14,8 +14,8 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-  IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-0d9d67af01c6a1199 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
-  echo "$i: $IP_ADDRESS"
+   IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-0d9d67af01c6a1199 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+   echo "$i: $IP_ADDRESS"
 
    aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
@@ -23,7 +23,7 @@ do
     {
         "Comment": "Creating a record set for cognito endpoint"
         ,"Changes": [{
-        "Action"              : "CREATE"
+         "Action"              : "CREATE"
         ,"ResourceRecordSet"  : {
             "Name"              : "'$i'.'$DOMAIN_NAME'"
             ,"Type"             : "A"
